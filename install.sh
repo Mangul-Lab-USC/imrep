083068d57d28e1ac37183d4ed285188b81f4fa61
Jaden <moorejaden5389@gmail.com>
2021-06-22 09:36:05 -0700

    Update install.sh to use requirements.txt
    This change gives us one source of truth for dependency versions and management.

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
-pip install pysam --user
-pip install biopython==1.76 --user
-pip install intervaltree --user
-pip install jellyfish==0.5.4 --user
-pip install numpy --user
-pip install networkx --user
+pip install -r requirements.txt
 cd suffix_tree
 python setup.py install --user
 cd ..
